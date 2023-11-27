25869f5bd245ebb54f0f779c665639b303cfc596
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-27 04:34:07 -0500

    new

---
+++ Javascript: The Hard Parts V2/shorthandway.js
@@ -0,0 +1,10 @@
+function multiplyBy2(input) {return input * 2}
+const multiplyBy2 = (input) => {return input * 2}
+
+const multiplyBy2 = (input) => input * 2
+
+const multiplyBy2 = input => input * 2
+
+const output = multiplyBy2(3)
+
+
